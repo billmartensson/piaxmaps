c222e0c423f86c8a8baaa3441e83c28f0d702102
Bill Mårtensson <bill@magictechnology.se>
2021-11-22 09:12:51 +0100

    Initial Commit

---
+++ piaxmaps/piaxmapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxmapsApp.swift
+//  piaxmaps
+//
+//  Created by Bill Martensson on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct piaxmapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
