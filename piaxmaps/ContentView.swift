810ea0bd63780ce183549f9f8b0ab91e277ca255
Bill Mårtensson <bill@magictechnology.se>
2021-11-22 11:32:55 +0100

    Position

--- piaxmaps/ContentView.swift
+++ piaxmaps/ContentView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
+import CoreLocationUI
 
 struct ContentView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 55.61259201984638, longitude: 12.993687792841314), span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+
+    
+    @StateObject var locationManager = LocationManager()
 
     
     var body: some View {
@@ -22,6 +27,15 @@ struct ContentView: View {
                         .frame(height: 300)
             
             
+            if let location = locationManager.location {
+                Text("Your location: \(location.latitude), \(location.longitude)")
+            }
+
+            LocationButton(.sendMyCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .frame(height: 44)
+            .padding()
         }
     }
 }
@@ -30,4 +44,45 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+
+    @Published var location: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+    }
+
+    func requestLocation() {
+        //manager.requestLocation()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let firstloc = locations.first {
+            if(firstloc.horizontalAccuracy > 100)
+            {
+                // Dålig signal
+                
+            }
+        }
+        
+        location = locations.first?.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print("Location något fel")
+        
+    }
+    
+    
+    
 }
